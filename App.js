f8cf2c1512f523cc019d6c8fb6fda076a7ca11c3
MohammedA-04 <mohammedahmed04@yahoo.com>
2024-05-30 22:33:47 +0100

    Project Setup

---
+++ App.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import AppNavigation from './navigation/appNavigation';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <AppNavigation/>
+    </>
+  );
+}
+
+
+
---
+++ screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { SafeAreaView, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+export default function HomeScreen() {
+    return(
+        <View className="flex-1 relative">
+            <StatusBar style="dark" />
+            {/* image placeholder */}
+
+            
+            <SafeAreaView>
+                <View className="mx-4 relative z-50">
+                    <Text>
+                        Hello Quran
+                    </Text>
+                </View>
+            </SafeAreaView>
+        </View>
+        
+    )
+}
