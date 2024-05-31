385d1f8f883a730d3d9b3510081d2f0454bf1e54
MohammedA-04 <mohammedahmed04@yahoo.com>
2024-05-31 23:55:27 +0100

    Adding new endpoint, chapter title and translation

--- api/quranAPI.js
+++ api/quranAPI.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 const BASE_URL = 'https://api.quran.com/api/v4/verses';
 const getRandomAyah_EP = `${BASE_URL}/random`;
 const getAyahbyKey_EP = (verse_key) => `${BASE_URL}/by_key/${verse_key}`;
+const getChapterList_EP  = 'https://api.quran.com/api/v4/chapters'
 
 // General API call function
 const apiCall = async (url, params = {}) => {
@@ -29,8 +30,9 @@ const apiCall = async (url, params = {}) => {
 // Fetch random Ayah with detailed fields
 export const fetchRandomAyah = () => {
     const params = {
-        translations: 'english',
+        translations: 'en.sahih',
         fields: 'text_uthmani,text_uthmani_simple,text_imlaei,text_indopak,words',
+        words: 'true'
 
     };
     return apiCall(getRandomAyah_EP, params);
@@ -41,10 +43,14 @@ export const fetchAyahByKey = (verse_key, additionalParams = {}) => {
     const params = {
         ...additionalParams,
         fields: 'text_uthmani,text_uthmani_simple,text_imlaei,text_indopak,words',
-        language: 'english',
-        words: 'true',
+        translations: 'en.sahih',
+        words: 'true'
     };
     // e.g., {BASE_URL}/by_key/{verse_key}
     const url = getAyahbyKey_EP(verse_key);
     return apiCall(url, params);
 };
+
+export const fetchChapterList = () => {
+    return apiCall(getChapterList_EP)
+}
